d22f84ab2bc97894fc1a10788f9a6d98fd5103b7
Frederik Panse <frederikpanse@protonmail.com>
2025-11-25 14:29:37 +0100

    Add make_baseline_csv notebook for processing output data

--- run_experiments/run_TROI_planning_agent.sh
+++ run_experiments/run_TROI_planning_agent.sh
@@ -20,11 +20,11 @@ do
     # Print the directory it's going to use
     echo -e "➤ Output directory: ${LOG_DIR}"
 
+    # add if using llm: agent.use_openrouter=true \
     python main.py \
         --config-dir=uncertain_worms/config/approaches/hardcoded \
         --config-name=lavawall_hardcoded_po_planning_agent.yaml \
         seed=$SEED \
-        # agent.use_openrouter=true \
         save_log=true \
         "hydra.run.dir=${LOG_DIR}" &
 
