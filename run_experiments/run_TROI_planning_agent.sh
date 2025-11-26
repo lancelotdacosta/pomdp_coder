31314a9dfdab3c3c1cb68ee118cbd6028ca6b809
Frederik Panse <frederikpanse@protonmail.com>
2025-11-26 11:48:56 +0100

    Update output directory and configuration for TROI planning agent to use tabular approach

--- run_experiments/run_TROI_planning_agent.sh
+++ run_experiments/run_TROI_planning_agent.sh
@@ -15,15 +15,15 @@ for SEED in {0..9}
 do
     echo -e "▶ Starting run for seed $SEED}"
 
-    LOG_DIR="outputs/lava/hardcoded/\${now:%Y-%m-%d}/\${now:%H-%M-%S}_seed${SEED}"
+    LOG_DIR="outputs/lava/tabular/\${now:%Y-%m-%d}/\${now:%H-%M-%S}_seed${SEED}"
 
     # Print the directory it's going to use
     echo -e "➤ Output directory: ${LOG_DIR}"
 
     # add if using llm: agent.use_openrouter=true \
     python main.py \
-        --config-dir=uncertain_worms/config/approaches/hardcoded \
-        --config-name=lavawall_hardcoded_po_planning_agent.yaml \
+        --config-dir=uncertain_worms/config/approaches/tabular \
+        --config-name=lava_tabular_po_planning_agent.yaml \
         seed=$SEED \
         save_log=true \
         "hydra.run.dir=${LOG_DIR}" &
