f84a7cb0c4572e31bf675763b28e20a47a1c7abf
Frederik Panse <frederikpanse@protonmail.com>
2025-12-16 11:33:32 +0100

    Clarify samples information in observation model prompt to emphasize input state context

--- uncertain_worms/policies/partially_obs_planning_agent.py
+++ uncertain_worms/policies/partially_obs_planning_agent.py
@@ -810,7 +810,7 @@ class LLMPartiallyObsPlanningAgent(
         )
 
         if model_name == "observation_model":
-            samples_info = "Below are samples of Actions and resulting Observations from the environment distribution. These are only samples from a larger distribution that you should model. \nNote: The samples do NOT show the underlying State because it is hidden.\nUse the samples and the environment specifications below to make an informed guess about the model.\n"
+            samples_info = "Below are samples of Actions and resulting Observations from the environment distribution. These are only samples from a larger distribution that you should model. \nNote: The samples do NOT show the underlying State because it is hidden.\nUse the samples and the environment specifications below to make an informed guess about the model.\nImportant: The input state is the state after the action has already been performed. Your goal is only to implement the sensor logic: Construct the observation (field of view) from the perspective of the state."
         else:
             samples_info = "Below are a few samples from the environment distribution. These are only samples from a larger distribution that you should model."
 
